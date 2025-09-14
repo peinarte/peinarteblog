a695c134d07f98dc96e9354b11b9e0e38cb33f43
peinarte <peinadosroot@gmail.com>
2025-09-14 19:19:51 -0400

    Create build-articles.js

---
+++ build-articles.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const articlesDir = path.join(__dirname, 'articles');
+const outputFile = path.join(__dirname, 'articles.json');
+
+fs.readdir(articlesDir, (err, files) => {
+  if (err) {
+    console.error("Error al leer la carpeta de artículos:", err);
+    return;
+  }
+
+  const allArticles = [];
+
+  files.forEach(file => {
+    if (path.extname(file) === '.json') {
+      const filePath = path.join(articlesDir, file);
+      const articleContent = fs.readFileSync(filePath, 'utf8');
+      allArticles.push(JSON.parse(articleContent));
+    }
+  });
+
+  fs.writeFileSync(outputFile, JSON.stringify(allArticles, null, 2), 'utf8');
+  console.log('Archivo articles.json creado con éxito.');
+});
